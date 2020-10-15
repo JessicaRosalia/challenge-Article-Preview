543d9cd6e08c2380e0caf051b4774fd6cc6249a1
jessicarf18 <jessicarosalia678@gmail.com>
2020-10-15 10:11:52 -0300

    versao desktop e animacao da versao mobile

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const avatar = document.querySelector(".avatar")
+const sectionShare = document.querySelector(".share")
+const network = document.querySelector(".network")
+const shareNetwork = document.querySelector(".share-network")
+
+shareNetwork.addEventListener("click", () => {
+    sectionShare.classList.toggle("teste")
+})
+
