7b84bb72f2de8dfbfe0df8fad40740f498bc6dde
jessicarf18 <jessicarosalia678@gmail.com>
2020-11-14 10:05:57 -0300

    Adicionando animação versão desktop

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+const body = document.querySelector("body")
 const avatar = document.querySelector(".avatar")
 const sectionShare = document.querySelector(".share")
 const network = document.querySelector(".network")
 const shareNetwork = document.querySelector(".share-network")
 
 shareNetwork.addEventListener("click", () => {
-    sectionShare.classList.toggle("teste")
+    if (body.clientWidth <= 768) {
+        sectionShare.classList.toggle("mobile-device")
+    }
+    else if (body.clientWidth >= 768) {
+        network.classList.toggle("desktop-device")
+    }
 })
 
